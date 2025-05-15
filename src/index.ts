c0fc0f843b9a05300684ff739fc20582ee2909fb
Bernard <benbaah104@gmail.com>
2025-05-15 09:55:36 +0000

    upate

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,12 @@ app.use(
 		credentials: true,
 	})
 );
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+});
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
